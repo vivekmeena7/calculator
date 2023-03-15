38f65f1ec48ec85caacd19e05bb0a82d6d8aab41
Vivek <vivek1322420@gmail.com>
2023-03-15 23:15:24 +0530

    Updates

---
+++ precache-manifest.56ed221caa1b68a4ed8a36acb1cb5c95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddda0e17a2d2caecec6cfba84622981",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/Calculator/static/css/2.f87fbcaa.chunk.css"
+  },
+  {
+    "revision": "d043634dc87f338d3480",
+    "url": "/Calculator/static/css/main.98df462f.chunk.css"
+  },
+  {
+    "revision": "58e3861956b6dcf89c5b",
+    "url": "/Calculator/static/js/2.9067ee69.chunk.js"
+  },
+  {
+    "revision": "d043634dc87f338d3480",
+    "url": "/Calculator/static/js/main.a6e5f47c.chunk.js"
+  },
+  {
+    "revision": "cf49d768c4d3a5b9fc25",
+    "url": "/Calculator/static/js/runtime~main.8078b280.js"
+  }
+]);
